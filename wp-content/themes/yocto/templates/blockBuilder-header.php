33e6bb85be41c6dddb5663a5450615722d162aad
Noel Hougland <noelhougland@gmail.com>
2017-08-03 11:29:34 -0700

    mobile header

--- wp-content/themes/yocto/templates/blockBuilder-header.php
+++ wp-content/themes/yocto/templates/blockBuilder-header.php
@@ -1,11 +1,13 @@
 <?php use Roots\Sage\Titles; ?>
-<div class="container">
-  <div class="content row page-content">
-    <div class="col-xs-12">
-		<div class="page-header">
-		 <h2 class="block-title"><?= Titles\title(); ?></h2>
+<div class="header-wrapper">
+	<div class="container">
+	  <div class="content row page-content">
+	    <div class="col-xs-12">
+			<div class="page-header">
+			 <h2 class="block-title"><?= Titles\title(); ?></h2>
+			</div>
 		</div>
+	  </div>
 	</div>
-  </div>
 </div>
 
